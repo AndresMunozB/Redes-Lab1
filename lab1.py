23a98a61042ab5cd165cedf5d5dad42f35a60324
Diego Mellis <diego.mellis@usach.cl>
2018-04-18 19:35:45 -0300

    Guardado de imagenes y correción del archivo de audio

--- lab1.py
+++ lab1.py
@@ -4,12 +4,17 @@ import numpy as np
 from numpy import sin, linspace, pi
 from scipy.io.wavfile import read, write
 from scipy import fft, ifft, arange
+from pylab import savefig
 import matplotlib.pyplot as plt
 
-#print(rate) ##frecuencia de muestreo
-#print(info)
-
-
+#==============================================================================
+# Función: En base a los datos que entrega beacon.wav se obtiene			   
+# los datos de la señal, la cantidad de datos que esta tiene, y el tiempo que  
+# que dura el audio.														   
+# Parámetros de entrada: Matriz con los datos de la amplitud del audio.
+# Parámetros de salida: Vector con la señal a trabajar, el largo de la señal y 
+# un vector con los tiempos de la señal.
+#==============================================================================
 def getDatos(info,rate):
 	#Datos del audio.
 	signal = info[:,0]
@@ -26,47 +31,91 @@ def getDatos(info,rate):
 	#print(t)
 	return signal, len_signal, t
 
+#=============================================================================
+# Función: Grafica los datos del audio en función del tiempo.
+# Parámetros de entrada: El arreglo con los datos del tiempo y los datos de la
+# señal.
+# Parámetros de salida: Ninguno, pero se muestra un gráfico por pantalla.
+#=============================================================================
 def graphTime(t, signal):
 	plt.plot(t,signal)
 	plt.title("Audio con respecto al tiempo")
 	plt.xlabel("Tiempo [s]")
 	plt.ylabel("Amplitud [dB]")
+	savefig("Audio con respecto al tiempo.png")
 	plt.show()
 
+#=============================================================================
+# Función: Función que se encarga de obtener la transformada de Fourier de los
+# datos de la señal.
+# Parámetros de entrada: Un arreglo con los datos de la señal, y el largo de 
+# este arreglo.
+# Parámetros de salida: Dos arreglos, uno con los valores del eje x y otro con
+# los valores del eje y.
+#=============================================================================
 def fourierTransformation(signal, len_signal):
 	fourierT = fft(signal)
 	fourierNorm = fourierT/len_signal
-	#print("FN", fourierNorm)
-	"""k = arange(len_signal)
-	tiempo = len_signal/rate
-	frq = k/tiempo"""
 	xfourier = np.fft.fftfreq(len(fourierNorm),1/rate)
 	return xfourier, fourierNorm
 
+
+#===============================================================================
+# Función: Grafica la transformada de Fourier, usando los arreglos de la función
+# anterior.
+# Parámetros de entrada: arreglo con los valores del eje x y arreglo con los 
+# valores del eje y.
+# Parámetros de salida: Ninguno, se muestra un gráfico por pantalla.
+#===============================================================================
 def graphTransformation(xfourier,yfourier):
     
 	plt.title("Amplitud vs Frecuencia")
 	plt.xlabel("Frecuencia [Hz]")
 	plt.ylabel("Amplitud [dB]")
 	plt.plot(xfourier,abs(yfourier))
+	savefig("Amplitud vs Frecuencia.png")
 	plt.show()
 
+#===============================================================================
+# Función: Obtiene la anti-transformada de fourier del arreglo yfourier.
+# Parámetros de entrada: Arreglo yfourier y el largo de este arreglo.
+# Parametros de salida: Un arreglo con los valores de la anti transformada de 
+# Fourier.
+#===============================================================================
 def getInverseFourier(yfourier,len_freq):
 		fourierTInv = ifft(yfourier)*len_freq
 		return fourierTInv
 
+#===============================================================================
+# Función: Grafica la anti-transformada de fourier.
+# Parámetros de entrada: Arreglo del tiempo y arreglo de la anti-transformada
+# de Fourier.
+# Parámetros de salida: Ninguno, se muestra un gráfico por pantalla.
+#===============================================================================
 def graphWithInverse(time, invFourier):
 	plt.title("Amplitud vs Inversa de Fourier")
 	plt.xlabel("Tiempo[s] (IFFT)")
 	plt.ylabel("Amplitud [dB]")
 	plt.plot(time,invFourier)
+	savefig("Amplitud vs Inversa de Fourier.png")
 	plt.show()
 
+#===============================================================================
+# Función: Obtiene el máximo del arreglo con los valores de Fourier.
+# Parámetros de entrada: Arreglo con los valores de la transformada de fourier.
+# Parámetros de salida: Valor máximo del arreglo.
+#===============================================================================
 def getMax(yfourier):
 	maxValue = max(yfourier)
-	#print("Maximo: ", maxValue)
 	return maxValue
 
+#========================================================================================
+# Función: Obtiene el índice del valor máximo que se entrega como parámetro,
+# dentro del arreglo yfourier.
+# Parámetros de entrada: Valor a buscar y arreglo yfourier (valores de las amplitudes de
+# la transformada de fourier)
+# Parámetros de salida: Indice del valor que se quería encontrar.
+#========================================================================================
 def getIndexValue(value, yfourier):
 	index = 0
 	for i in range(len(yfourier)):
@@ -74,42 +123,45 @@ def getIndexValue(value, yfourier):
 			index = i
 	return i
 
+#========================================================================================
+# Función: Esta función hace el truncamiento del 15% de los datos, para reducir el ruido
+# de la señal.
+# Parámetros de entrada: Valor máximo del arreglo de las amplitudes (eje Y de la transformada)
+# Parámetros de salida: El arreglo de las amplitudes pero con el ruido reducido.
+#========================================================================================
 def removeNoise(maxValue, yfourier):
 	n_amplitude = len(yfourier)
-	n_amplitude = int(n_amplitude/2)
+	n_amplitude = int(n_amplitude)
 	
-	#print("n_amplitude: ",n_amplitude)
 	fifteen_percent_ampl = n_amplitude*0.15
 	fifteen_percent_ampl = int(fifteen_percent_ampl)
-	#print("fifteen: ", fifteen_percent_ampl)
-	withoutNoise = np.zeros(n_amplitude*2,np.complex256)
+	withoutNoise = np.zeros(n_amplitude,np.complex256)
 	pos = getIndexValue(maxValue,yfourier)
-	#print("Indice: ", pos)
 	min_pos = pos - fifteen_percent_ampl
 	max_pos = pos + fifteen_percent_ampl
-	#print("Posicion minima: ", min_pos)
-	#print("Posicion maxima: ", max_pos)
-	#print(len(yfourier))
-	#print(len(withoutNoise))
-	#print("Without Noise:", withoutNoise)
 	withoutNoise[min_pos:max_pos] = yfourier[min_pos:max_pos]
 	return withoutNoise
-	
-def graphWithoutNoise(t,inverseWithoutNoise):
-    plt.plot(t,inverseWithoutNoise,"--")
-    plt.title("Audio con respecto al tiempo sin ruido (ifft)")
-    plt.xlabel("Tiempo [s] (IFFT)")
-    plt.ylabel("Amplitud [dB]")
-    plt.show()
 
+#================================================================================================
+# Función: Grafica la señal sin ruido.
+# Parámetros de entrada: Arreglo de los valores del tiempo y arreglo datos de la señal sin ruido.
+# Parámetros de salida: Ninguno, pero muestra un gráfico por pantalla.
+#===============================================================================================	
+def graphWithoutNoise(t,inverseWithoutNoise):
+	plt.plot(t,inverseWithoutNoise,"--")
+	plt.title("Audio con respecto al tiempo sin ruido (ifft)")
+	plt.xlabel("Tiempo [s] (IFFT)")
+	plt.ylabel("Amplitud [dB]")
+	savefig("Audio con respecto al tiempo sin ruido (ifft).png")
+	plt.show()
 
 def showMenu():
-	print("\n\n    MENU\n\n")
-	print("1) Gráfico del audio original: Amplitud vs Tiempo")
-	print("2) Gráfico de la Transformada de Fourier: Amplitud vs Frecuencia")
-	print("3) Gráfico de la Anti-Transformada fourier : Amplitud vs Tiempo (IFFT) ")
-	print("4) Gráfico de la Tranformada truncado al 15%: Amplitud vs Frecuencia")
-	print("5) Grafico de la Anti-Transformada truncado al 15%: Amplitud vs Frecuencia")
+	print("\n\t\t\tMENU PARA SELECCIÓN DE GRÁFICOS\n")
+	print("   1) Gráfico del audio original: Amplitud vs Tiempo")
+	print("   2) Gráfico de la Transformada de Fourier: Amplitud vs Frecuencia")
+	print("   3) Gráfico de la Anti-Transformada fourier : Amplitud vs Tiempo (IFFT) ")
+	print("   4) Gráfico de la Tranformada truncado al 15%: Amplitud vs Frecuencia")
+	print("   5) Grafico de la Anti-Transformada truncado al 15%:Amplitud vs Frecuencia")
 
 rate,info = read("beacon.wav")
 señal, largo_señal, t = getDatos(info,rate)
@@ -132,6 +184,6 @@ while menu != str(6):
 		graphTransformation(xfourier,without_noise)
 	elif(menu == "5"):
 		graphWithoutNoise(t, inv_without_noise)
-		write("beacon3.wav",rate,inv_without_noise)
+		write("beacon2.wav",rate,inv_without_noise.astype(info.dtype))
 	else:
 		print("Opción inválida")
